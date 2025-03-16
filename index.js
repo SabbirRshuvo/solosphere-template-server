655334470292f11194b6546b494536ce53407f06
SabbirRshuvo <sabbirhshuvo11@gmail.com>
2025-03-17 00:44:14 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,37 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const port = process.env.PORT || 3000;
+const { MongoClient, ServerApiVersion } = require("mongodb");
+// middleware
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.hrmuz.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    console.log("mongodb connected");
+
+    app.get("/", async (req, res) => {
+      res.send("server is running");
+    });
+    app.listen(port, () => {
+      console.log(`running on the port : ${port}`);
+    });
+  } catch (error) {
+    console.log("Mongodb connected error");
+  }
+}
+run();
